284f051079689d21b501e6955aeba32e19c43b9b
Marcos Pérez <60829967+Qhumir@users.noreply.github.com>
2020-06-19 19:38:32 -0500

    final script with everything required

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,8 @@
 ######## Libraries
 
 library(dplyr)
+#library(plyr)
+library(reshape2)
 
 
 
@@ -14,6 +16,7 @@ features <- read.table(file="./UCI HAR Dataset/features.txt")
 
 # Reading labels
 act_labels <- read.table(file="./UCI HAR Dataset/activity_labels.txt")
+act_labels <- rename(act_labels, CodeAct = V1, Activity = V2)
 
 # Reading train data
 train.x <- read.table(file="./UCI HAR Dataset/train/X_train.txt")
@@ -29,8 +32,56 @@ test.s <- read.table(file="./UCI HAR Dataset/test/subject_test.txt")
 
 ######## Constructting tidy training data
 
+# Change to describing names
+names(train.x) <- features$V2 %>% gsub(",|-|\\(|\\)", "\\.",.)  %>% gsub("\\.\\.","\\.",.) %>% 
+                    gsub("\\.\\.","\\.",.) %>% gsub("\\.$","",.)
+
+# rename and join training data
+train <- cbind(rename(train.s, id = V1), rename(train.y, CodeAct = V1), train.x)
+
+
 
 ######## Constructting tidy test data
 
+# Change to describing names
+names(test.x) <- features$V2 %>% gsub(",|-|\\(|\\)", "\\.",.)  %>% gsub("\\.\\.","\\.",.) %>% 
+  gsub("\\.\\.","\\.",.) %>% gsub("\\.$","",.)
 
+# rename and join test data
+test<- cbind(rename(test.s, id = V1), rename(test.y, CodeAct = V1), test.x)
+
+
+
+######## 1. Merges the training and the test sets to create one data set.
+
+Data <- rbind(train, test)
+
+
+
+######## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+meltData <- melt(Data, id = c("CodeAct", "id"),measure.vars = names(test.x))
+vars_mean_sd <- do.call(rbind,lapply(split(meltData,meltData$variable), summarize, 
+                  mean = mean(value), sd = sd(value)))
+
+
+
+######## 3. Uses descriptive activity names to name the activities in the data set.
+
+# The columns names of act_labels were changed before the first step
+Data <- merge(act_labels, Data, by = "CodeAct")
+
+
+
+######## 4. Appropriately labels the data set with descriptive variable names.
+
+# It was done before the first step
+
+
+
+######## 5. From the data set in step 4, creates a second, independent tidy data set with the average
+#           of each variable for each activity and each subject.
+
+meltData <- melt(Data, id = c("CodeAct", "Activity", "id"),measure.vars = names(test.x))
+ActData <- dcast(meltData, Activity ~ variable, mean)
 
